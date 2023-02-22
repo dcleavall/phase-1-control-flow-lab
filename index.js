e0bcb9801d20d979e6c90300d76a71aa832a8ad0
Derrick <dcleavall@gmail.com>
2023-02-22 07:09:34 -0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let driverGreeting
+  if (feet < 400) {
+    driverGreeting = 'This one is on me!';
+    return driverGreeting
+  }
+  else if (feet > 400 && feet < 2000  ) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+   }
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+  case 'generous':
+    return "Thank you so much."
+    break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+      case 'thanks for everything':
+        return "Bye."
+}}
